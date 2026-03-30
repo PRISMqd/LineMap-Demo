78798f2cdc32c83c01f7e618c2fe840605c8bf73
JPQD <101489715+jkt78@users.noreply.github.com>
2026-03-30 15:04:41 -0400

    Delete components directory

--- components/PatientList.js
+++
@@ -1,8 +0,0 @@
-
-export default function PatientList({patients,onSelect}){
-return(<div className="list">
-{patients.map(p=><div key={p.id} className="patient" onClick={()=>onSelect(p)}>
-<strong>{p.name}</strong><br/>Room {p.room}
-</div>)}
-</div>);
-}
